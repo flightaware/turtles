889c9c35538a913c9854d5cecc9d23801eb7ecb6
Michael Yantosca <michael.yantosca@flightaware.com>
2019-07-08 17:35:25 -0500

    FT-5294: Augment ::turtles::on_proc_enter to grab caller and callee information.
    	The facilities in Tcl for introspecting caller/callee information is a little roundabout.
    	One must mind the stack position and not forget to account for the instrumenting function.
    
    	After a brief assay in trying to follow the tcl/curry page, a simpler method was found.
    	Note that the magic number (-2) demands that the frame fetch happen inside ::turtles::on_proc_enter,
    	and not in some abstracted helper function.

--- turtles.tcl
+++ turtles.tcl
@@ -7,12 +7,33 @@ namespace eval ::turtles {
 }
 
 proc ::turtles::on_proc_enter {commandString op} {
-	puts stderr "\[[clock microseconds]\] ($op) [format {%s} $commandString]"
+	set time_enter [ clock microseconds ]
+	# Retrieve the frame two levels down the call stack to avoid
+	# confusing with the stack frame for ::turtles::on_proc_enter.
+	set execFrame [info frame -2]
+	if { [dict exists $execFrame proc] } {
+		# Called from within procedure
+		set callerName [dict get $execFrame proc]
+	} else {
+		# Called from top level
+		set callerName ""
+	}
+	# Callee needs to be fully qualified for consistency.
+	set calleeName [namespace which -command [dict get $execFrame cmd]]
+	# Get hashes on FQFNs for caller and callee.
+	set callerId [ ::turtles::hash $callerName ]
+	set calleeId [ ::turtles::hash $calleeName ]
+	# Record entry into proc.
+	puts stderr "\[$time_enter\] ($op) $callerName ($callerId) -> $calleeName ($calleeId)"
 }
 
 proc ::turtles::on_proc_define_add_trace {commandString code result op} {
-	set procName [lindex [split $commandString { }] 1]
-	catch { trace add execution $procName [list enter] turtles::on_proc_enter }
+	# Proc name needs to be fully qualified for consistency.
+	set procName [namespace which -command [lindex [split $commandString { }] 1]]
+	# Add handler for proc entry.
+	if { [ catch { trace add execution $procName [list enter] ::turtles::on_proc_enter } err ] } {
+		puts "Failed to add enter trace for $procName : $err"
+	}
 }
 
 proc ::turtles::release_the_turtles {} {
